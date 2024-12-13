1bed17ba35c1ffd9d81276502a8f9c23d13924d5
Dominik Thomann <dthomann@freesurf.ch>
2024-12-13 15:10:09 +0100

    Initial Commit

---
+++ InnertiaStopper/InnertiaStopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnertiaStopperApp.swift
+//  InnertiaStopper
+//
+//  Created by Dominik Thomann (privat) on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct InnertiaStopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InnertiaStopperTests/InnertiaStopperTests.swift
@@ -0,0 +1,17 @@
+//
+//  InnertiaStopperTests.swift
+//  InnertiaStopperTests
+//
+//  Created by Dominik Thomann (privat) on 13.12.2024.
+//
+
+import Testing
+@testable import InnertiaStopper
+
+struct InnertiaStopperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
